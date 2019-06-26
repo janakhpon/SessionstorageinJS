b9435027e4685a351313597a2a9bd9b99d7771d1
janakhpon <jnovaxer@gmail.com>
2019-06-26 15:40:47 +0630

    GDEV004

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavbarCon from './components/Navbar';
+import Landing from './components/Landing';
+import Sessiontask from './components/Sessiontask';
 import Localtask from './components/Localtask';
 import './App.css';
 
 function App() {
   return (
-    <div className="App app-container">
-      <Localtask/>
-    </div>
+    <Router>
+      <div className="app-container">
+        <NavbarCon />
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/session' component={Sessiontask} />
+        <Route exact path='/local' component={Localtask} />
+      </div>
+    </Router>
   );
 }
 
